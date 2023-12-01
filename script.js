c4524beb30572e0e0dde7ee5193ea731d8551577
Youdi <mehdi.youssef97@gmail.com>
2023-12-01 15:08:17 -0500

    fixed ui, still has bug where game does not end

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
+
 //declaring variables
-let playerLives = 5;
-let computerLives = 5;
-let playerSelection = '';
-let computerSelection = '';
+let playerLives = 5
+let computerLives = 5
+let playerSelection = ''
+let computerSelection = ''
 
 //settings possible options
-options = ["rock", "paper", "scissors"];
+options = ["rock" , "paper", "scissors"]
 
 //linking js to html elements
-const rockButton = document.querySelector("#rock");
-const paperButton = document.querySelector("#paper");
-const scissorsButton = document.querySelector("#scissors");
-const result = document.querySelector("#resultContainer");
-const resetButton = document.querySelector(".resetContainer");
+const rockButton = document.querySelector("#rock")
+const paperButton = document.querySelector("#paper")
+const scissorsButton = document.querySelector("#scissors")
 
-const winLoseText = document.querySelector("#winLoseText");
-const winnerText = document.querySelector("#winnerText");
-const playerLivesText = document.querySelector("#playerLivesText");
-const computerLivesText = document.querySelector("#computerLivesText");
+//linking text elements
+const winLoseText = document.querySelector("#winLoseText")
+const playerLivesText = document.querySelector("#playerLivesText")
+const computerLivesText = document.querySelector("#computerLivesText")
+const playerChoiceIcon = document.querySelector("#playerChoice")
+const computerChoiceIcon = document.querySelector("#computerChoice")
 
 //initiating player and computer lives as 5
-playerLivesText.textContent = playerLives;
-computerLivesText.textContent = computerLives;
+playerLivesText.textContent = playerLives
+computerLivesText.textContent = computerLives
 
 //function to choose random option from options array
 function getComputerSelection()
 {
-    choice = Math.floor(Math.random() * 3); //random number between 0 and 2
-    return options[choice];
+    choice = Math.floor(Math.random() * 3) //random number between 0 and 2
+    return options[choice]
 }
 
-//
+//function to get player selection based on image clicked
 function getPlayerSelection(playerClicked)
 {
     playerSelection = playerClicked;
-    computerSelection = getComputerSelection();
-    playRound(playerSelection, computerSelection);
+    computerSelection = getComputerSelection()
+    playRound(playerSelection, computerSelection)
 }
 
 //function to check if game is over
@@ -43,33 +44,27 @@ function endGame()
 {
     if (computerLives == 0)
     {
-        winnerText.textContent = 'Player Wins!';
+        winLoseText.textContent = 'Player Wins!'
     }
     else if(playerLives == 0)
     {
-        winnerText.textContent = 'Computer Won!'
+        winLoseText.textContent = 'Computer Won!'
     }
-
 }
 
 //event listener for when player presses rock
 rockButton.addEventListener('click', () => {
-    getPlayerSelection(options[0]);
+    getPlayerSelection(options[0])
 });
 
 //event listener for when player presses paper
 paperButton.addEventListener('click', () => {
-    getPlayerSelection(options[1]);
+    getPlayerSelection(options[1])
 });
 
 //event listener for when player presses scissors
 scissorsButton.addEventListener('click', () => {
-    getPlayerSelection(options[2]);
-});
-
-//event listener for when player presses reset button which will reload the page
-resetButton.addEventListener('click', () => {
-    location.reload();
+    getPlayerSelection(options[2])
 });
 
 //function where choices are compared to see who won the round
@@ -78,7 +73,7 @@ function playRound(realPlayer, computerPlayer)
     //tie conditions
     if (realPlayer == computerPlayer)
     {
-        winLoseText.textContent = 'Round \n Tied';
+        winLoseText.textContent = 'Round \n Tied.'
 
     //winning conditions 
     } else if ((realPlayer == "rock" && computerPlayer == "scissors") || 
@@ -86,19 +81,13 @@ function playRound(realPlayer, computerPlayer)
     (realPlayer == "scissors" && computerPlayer == "paper"))
     {
         computerLives -= 1
-        computerLivesText.textContent = computerLives;
-        winLoseText.textContent = 'Round \n Won';
-
-        //reset lives to 0 if below 0 CURRENTLY NOT WORKING
-        if (computerLives < 0)
-        {
-            computerLives = 0;
-        }
+        computerLivesText.textContent = computerLives
+        winLoseText.textContent = 'Round \n Won!'
 
         //calls endgame function if computer is at 0 lives
         if(computerLives == 0)
         {
-            endGame();
+            endGame()
         }
     }
 
@@ -108,19 +97,44 @@ function playRound(realPlayer, computerPlayer)
     (realPlayer == "scissors" && computerPlayer == "rock"))
     {
         playerLives -= 1
-        playerLivesText.textContent = playerLives;
-        winLoseText.textContent = 'Round \n Lost';
-        
-        //reset lives to 0 if below 0 CURRENTLY NOT WORKING
-        if (playerLives < 0)
-        {
-            playerLives = 0;
-        }
+        playerLivesText.textContent = playerLives
+        winLoseText.textContent = 'Round \n Lost...'
 
         //calls endgame function if player is at 0 lives
         if(playerLives == 0)
         {
-            endGame();
+            endGame()
         }
+    }
+
+    setChoiceIcon(playerSelection, computerSelection)
+}
+
+function setChoiceIcon(playerSelection, computerSelection)
+{
+    switch (playerSelection)
+    {
+        case 'rock':
+            playerChoiceIcon.textContent = '✊'
+            break
+        case 'paper':
+            playerChoiceIcon.textContent = '✋'
+            break
+        case 'scissors':
+            playerChoiceIcon.textContent = '✌️'
+            break
+    }
+
+    switch (computerSelection)
+    {
+        case 'rock':
+            computerChoiceIcon.textContent = '✊'
+            break
+        case 'paper':
+            computerChoiceIcon.textContent = '✋'
+            break
+        case 'scissors':
+            computerChoiceIcon.textContent = '✌️'
+            break
     }
 }
